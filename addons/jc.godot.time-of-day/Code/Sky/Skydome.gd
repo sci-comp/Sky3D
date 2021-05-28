444128d3ce25d8f982cef47bd71f7d5d2a4993a4
7leodev <jleodevelp@gmail.com>
2021-05-28 04:15:36 -0600

    Added skydome resources and sky const-gdscript

--- addons/jc.godot.time-of-day/Code/Sky/Skydome.gd
+++ addons/jc.godot.time-of-day/Code/Sky/Skydome.gd
@@ -1,3 +1,4 @@
+tool
 class_name Skydome extends Node
 """========================================================
 °                         TimeOfDay.
@@ -18,3 +19,34 @@ var sun_azimuth: float
 
 var moon_altitude: float 
 var moon_azimuth: float
+
+
+## Resources and instances.
+
+# Resources.
+var __resources:= SkydomeResources.new()
+
+# Instances.
+var __sky_instance: MeshInstance = null
+var __fog_instance: MeshInstance = null
+var __moon_instance: Viewport = null
+var __moon_rt: ViewportTexture = null
+var __moon_instance_transform: Spatial = null
+var __moon_instance_mesh: MeshInstance = null
+var __clouds_cumulus_instance: MeshInstance = null
+
+func __check_instances() -> bool:
+	__sky_instance = get_node_or_null(SkyConst.SKY_INSTANCE)
+	__moon_instance = get_node_or_null(SkyConst.MOON_INSTANCE)
+	__fog_instance = get_node_or_null(SkyConst.FOG_INSTANCE)
+	__clouds_cumulus_instance = get_node_or_null(SkyConst.CLOUDS_C_INSTANCE)
+	
+	if __sky_instance == null: return false
+	if __moon_instance == null: return false
+	if __fog_instance == null: return false
+	if __clouds_cumulus_instance == null: return false
+	
+	return true
+
+"""
+"""
